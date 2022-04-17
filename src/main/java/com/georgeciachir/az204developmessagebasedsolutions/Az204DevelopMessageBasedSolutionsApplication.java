d0c806119bf7b091505bcbdf85d67dbdba6e79c9
GeorgeCiachir <george.ciachir@yahoo.com>
2022-04-17 11:04:49 +0300

    initial commit

---
+++ src/main/java/com/georgeciachir/az204developmessagebasedsolutions/Az204DevelopMessageBasedSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.georgeciachir.az204developmessagebasedsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Az204DevelopMessageBasedSolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Az204DevelopMessageBasedSolutionsApplication.class, args);
+    }
+
+}
